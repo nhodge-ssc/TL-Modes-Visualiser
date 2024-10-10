418b3cf2acc42c2309b44420545db2734943b4ed
nhodge-ssc <nate.hodge@sscspace.com>
2024-10-10 15:14:02 -0800

    Update TM.py
    Updating to bring in line with changes to polar plotting on TE.py in Python 3.12 with matplotlib 3.9.

--- TM.py
+++ TM.py
@@ -72,22 +72,20 @@ def handleTM(st, modes=[0, 0], type_of_waveguide="Rectangular", A=10, B=5, R=5):
         U = -1 * special.jvp(N, X[-1].round(3) / R * RAD) * np.cos(N * T)
         V = special.jv(N, X[-1].round(3) / R * RAD) * np.sin(N * T)
         par = Circular_TE_TM_Functions(N, P, 2.3e-2)
-        fig, ax = plt.subplots()
 
-        plt.polar(2 * PI, R)
-        plt.streamplot(T, RAD, V, U, color="xkcd:azure")
-        plt.axis("scaled")
+        fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})  # modified to give polar plot in matplotlib 3.9
+        ax.streamplot(T, RAD, V, U, color="xkcd:azure", density=1.2)  #increasing density above default of 1 gives slightly more streamlines to fill out higher order modes better.
+        ax.set_rlim(0, R)  #sets radius axis to be only from 0 to max radius
         st.subheader("E field")
         st.pyplot(fig)
         st.markdown("**Scale: 5units = 2.3 cm**")
 
         U = special.jv(N, X[-1].round(3) / R * RAD) * np.sin(N * T)
         V = special.jvp(N, X[-1].round(3) / R * RAD) * np.cos(N * T)
-
-        fig, ax = plt.subplots()
-        plt.polar(2 * PI, R)
-        plt.streamplot(T, RAD, V, U, color="red")
-        plt.axis("scaled")
+        
+        fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})  # modified to give polar plot in matplotlib 3.9
+        ax.streamplot(T, RAD, V, U, color="red", density=1.2)  #increasing density above default of 1 gives slightly more streamlines to fill out higher order modes better.
+        ax.set_rlim(0, R)  #sets radius axis to be only from 0 to max radius
         st.subheader("H field")
         st.pyplot(fig)
         st.markdown("**Scale: 5units = 2.3 cm**")
